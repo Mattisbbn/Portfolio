df43b4a34db00eb622d23688b2a8a6bbde27657d
Mattisbbn <mattisbbn@gmail.com>
2024-05-03 15:49:54 +0200

    Update

--- pages/entreprise/entreprise.php
+++ pages/entreprise/entreprise.php
@@ -1,5 +1,11 @@
-<section class=" section" id=""> 
+<section class="entreprise section" id=""> 
 <h2 class="section_title">Entreprise</h2>
     <span class="section_subtitle">Mon entreprise</span>
 
+
+    <h3 class="option-h3">Description</h3>
+    <p class="option-text"> MyMicro est une entreprise basée à Angers, en France, spécialisée dans le reconditionnement et la vente de matériel informatique. Fondée en 2006, elle se distingue par son engagement en faveur de l'économie circulaire et de l'insertion sociale. En reconditionnant des équipements électroniques et en offrant des opportunités d'emploi à des personnes en difficulté, elle contribue à réduire les déchets et à favoriser l'inclusion professionnelle.</p>
+
+    <h3 class="option-h3">Chiffres Clés</h3>
+    <p class="option-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis doloremque, maiores dicta, obcaecati rem voluptatem, eos aliquam eveniet magnam vitae vero officiis molestiae! Aliquid, dicta nobis pariatur eaque omnis dolores!</p>
 </section>
--- pages/repertoire/repertoire.php
+++ pages/repertoire/repertoire.php
@@ -16,6 +16,24 @@
                     <td>Utilisation JS</td>
                     <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sequi molestias sint in, ducimus deleniti. Explicabo distinctio quod dolore aliquam, labore architecto, amet consequuntur ullam facere, qui molestias. Enim, facere.</td>
                 </tr>
+                <tr>
+                    <th scope="row">Projet</th>
+                    <td>Calculatrice JS</td>
+                    <td>Utilisation JS</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sequi molestias sint in, ducimus deleniti. Explicabo distinctio quod dolore aliquam, labore architecto, amet consequuntur ullam facere, qui molestias. Enim, facere.</td>
+                </tr>
+                <tr>
+                    <th scope="row">Projet</th>
+                    <td>Calculatrice JS</td>
+                    <td>Utilisation JS</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sequi molestias sint in, ducimus deleniti. Explicabo distinctio quod dolore aliquam, labore architecto, amet consequuntur ullam facere, qui molestias. Enim, facere.</td>
+                </tr>
+                <tr>
+                    <th scope="row">Projet</th>
+                    <td>Calculatrice JS</td>
+                    <td>Utilisation JS</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sequi molestias sint in, ducimus deleniti. Explicabo distinctio quod dolore aliquam, labore architecto, amet consequuntur ullam facere, qui molestias. Enim, facere.</td>
+                </tr>
             </tbody>
         </table>
     </div>
