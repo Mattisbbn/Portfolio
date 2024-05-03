314383771f5b8f3560c47e9c3f917bb12c87b962
Mattisbbn <mattisbbn@gmail.com>
2024-05-03 14:12:35 +0200

    Update

--- pages/entreprise/entreprise.php
+++ pages/entreprise/entreprise.php
@@ -1,1 +1,5 @@
-erghberi
+<section class=" section" id=""> 
+<h2 class="section_title">Entreprise</h2>
+    <span class="section_subtitle">Mon entreprise</span>
+
+</section>
--- pages/repertoire/repertoire.php
+++ pages/repertoire/repertoire.php
@@ -1,32 +1,22 @@
-<section class="repertoire">
-
-
-<table>
-    <thead>
-    <th scope="col">Type</th>
-    <th scope="col">Nom</th>
-    <th scope="col">Compétences</th>
-    <th scope="col">Sous-compétences</th>
-    </thead>
-    <tbody>
-    <tr>  
-        <th scope="row">Projet</th>
-        <td>Calculatrice JS</td>
-        <td>Utilisation JS</td>
-        <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sequi molestias sint in, ducimus deleniti. Explicabo distinctio quod dolore aliquam, labore architecto, amet consequuntur ullam facere, qui molestias. Enim, facere.</td>
-    </tr>
-      
-    </tbody>
-
-
-</table>
-
-
-
-
-
-
-
-
-
+<section class="project section repertoire " id="project">
+    <h2 class="section_title">Repertoire</h2>
+    <span class="section_subtitle">Tableau de synthèse</span>
+    <div class="repertoire-container">
+        <table>
+            <thead>
+                <th scope="col">Type</th>
+                <th scope="col">Nom</th>
+                <th scope="col">Compétences</th>
+                <th scope="col">Sous-compétences</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Projet</th>
+                    <td>Calculatrice JS</td>
+                    <td>Utilisation JS</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sequi molestias sint in, ducimus deleniti. Explicabo distinctio quod dolore aliquam, labore architecto, amet consequuntur ullam facere, qui molestias. Enim, facere.</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </section>
