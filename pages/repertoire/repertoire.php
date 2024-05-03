2f88fe0ac10f838788c94a8ae1a52f1494702253
Mattisbbn <mattisbbn@gmail.com>
2024-05-03 13:39:20 +0200

    update

--- pages/repertoire/repertoire.php
+++ pages/repertoire/repertoire.php
@@ -1,1 +1,32 @@
-zefkzefkzfpizje
+<section class="repertoire">
+
+
+<table>
+    <thead>
+    <th scope="col">Type</th>
+    <th scope="col">Nom</th>
+    <th scope="col">Compétences</th>
+    <th scope="col">Sous-compétences</th>
+    </thead>
+    <tbody>
+    <tr>  
+        <th scope="row">Projet</th>
+        <td>Calculatrice JS</td>
+        <td>Utilisation JS</td>
+        <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sequi molestias sint in, ducimus deleniti. Explicabo distinctio quod dolore aliquam, labore architecto, amet consequuntur ullam facere, qui molestias. Enim, facere.</td>
+    </tr>
+      
+    </tbody>
+
+
+</table>
+
+
+
+
+
+
+
+
+
+</section>
