fba357ff2005863417e813d1d3cd3055297a66de
Mattisbbn <mattisbbn@gmail.com>
2024-10-04 09:35:38 +0200

    Update router

--- index.php
+++ index.php
@@ -4,18 +4,19 @@
     <main class="main">
         <?php require_once('./partials/header.php');
 
-
-
-        foreach ($_GET as $key => $value) {
-            if (file_exists('pages/travaux/'.$key.'.php')) {
-                require_once('pages/travaux/'.$key.'.php');
-            } elseif (file_exists('./pages/'.$key.'/'.$key.'.php')) {
-                require_once('./pages/'.$key.'/'.$key.'.php');
-            } else {
-                require_once('./pages/accueil/accueil.php');
+        if(!empty($_GET)){
+            foreach ($_GET as $key => $value) {
+                if (file_exists('pages/travaux/'.$key.'.php')) {
+                    require_once('pages/travaux/'.$key.'.php');
+                } elseif (file_exists('./pages/'.$key.'/'.$key.'.php')) {
+                    require_once('./pages/'.$key.'/'.$key.'.php');
+                } else {
+                    require_once('./pages/accueil/accueil.php');
+                }
             }
+        }else{
+            require_once('./pages/accueil/accueil.php');
         }
-
         require_once('./partials/footer.php');
 
         ?>
