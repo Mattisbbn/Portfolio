412772a0a3679f0b72694d3b6509794ecd5250eb
Mattisbbn <mattisbbn@gmail.com>
2025-01-08 09:40:33 +0100

    Update travaux

--- view/travaux/calculatrice-js/calculatrice-js.php
+++ view/travaux/calculatrice-js/calculatrice-js.php
@@ -1,8 +1,8 @@
 <section class="full-h">
-    <h1 class="text-center fw-semibold mt-4 mb-4 pt-2 page-title">Intégration maquette Apple</h1>
+    <h1 class="text-center fw-semibold mt-4 mb-4 pt-2 page-title">Calculatrice JS</h1>
     
     <div class="col-12 d-flex">
-        <img class="col-11 col-xl-6 mt-4 m-auto project-img rounded-4" src="public/img/projects/apple.png" alt="">
+        <img class="col-11 col-xl-6 mt-4 m-auto project-img rounded-4" src="public/img/projects/calculatrice-js.png" alt="">
     </div>
   
     <h3 class="text-center fw-semibold">Tâches réalisées</h3>
