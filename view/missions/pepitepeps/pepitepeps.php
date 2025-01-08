28bf5569491629f52224cf8eb6201371153198f8
Mattisbbn <mattisbbn@gmail.com>
2025-01-08 14:13:18 +0100

    More projects

---
+++ view/missions/pepitepeps/pepitepeps.php
@@ -0,0 +1,13 @@
+<section class="full-h">
+    <h1 class="text-center fw-semibold mt-4 mb-4 pt-2 page-title">PéPite PeP's</h1>
+    
+    <div class="col-12 d-flex">
+        <img class="col-11 col-xl-6 mt-4 m-auto project-img rounded-4" src="public/img/projects/pepitepeps.png" alt="">
+    </div>
+  
+    <h3 class="text-center fw-semibold">Tâches réalisées</h3>
+    <h3 class="text-center fw-semibold">Moyens utilisés</h3>
+    <h3 class="text-center fw-semibold">Contexte</h3>
+
+
+</section>
