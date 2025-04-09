0c71362a8c949c1027423ad4bb91f0c83b9b4594
Mattisbbn <mattisbbn@gmail.com>
2025-04-09 10:22:05 +0200

    update

---
+++ view/projets/clickneat/clickneat.php
@@ -0,0 +1,64 @@
+<section class="full-h">
+    <h1 class="text-center fw-semibold mt-4 mb-4 pt-2 page-title">Click&Eat</h1>
+    
+    <div class="col-12 d-flex">
+        <img class="col-11 col-xl-6 mt-4 m-auto project-img rounded-4" src="/public/img/projects/ovnyl.jpg" alt="">
+    </div>
+  
+    <div class="col-6 m-auto">
+      
+
+        <h3 class="text-center fw-semibold my-4">Contexte</h3>
+        <p class="dynamic-paragraph ">La startup Click’n Eat nous mandate pour réaliser une application de réservation de restaurant avec prise de commande au préalable, comme un “restaurant sans attente”. Le repas est donc prêt dès notre arrivée.</p>
+
+        <h3 class="text-center fw-semibold  my-4">Objectifs</h3>
+        <p class="dynamic-paragraph">Proposer une solution optimisée pour plusieurs restaurants qui proposent des menus différents.</p>
+        <p class="dynamic-paragraph">Optimiser les échanges entre les différents acteurs du restaurant (administration, service, cuisine, clients).</p>
+        
+
+        <h3 class="text-center fw-semibold  my-4">Expression des besoins</h3>
+        <p class="dynamic-paragraph ">Une gestion complète et optimisée des restaurants, permettant de suivre en temps réel les menus, les commandes et les réservations. L’ensemble des opérations est parfaitement synchronisé avec les horaires d’ouverture, assurant une expérience fluide et sur-mesure pour chaque client. L’application Click’n Eat permet de répondre à ces besoins en offrant les outils nécessaires à l’ajustement des offres, à la gestion des réservations et au suivi des commandes, accessibles en ligne pour un pilotage efficace des établissements à tout moment.</p>
+
+        <h3 class="text-center fw-semibold  my-4">Fonctionnalités MVP (Minimum Viable Product)</h3>
+            <ul>
+                <li><p class="dynamic-paragraph ">Gestion des restaurants </p></li>
+                <li><p class="dynamic-paragraph ">Authentification des utilisateurs </p></li>
+                <li><p class="dynamic-paragraph ">Accès spécifique restaurant </p></li>
+                <li><p class="dynamic-paragraph ">Accès spécifique client </p></li>
+                <li><p class="dynamic-paragraph ">Gestion de la carte/des menus (classification des articles au menu par catégorie) </p></li>
+                <li><p class="dynamic-paragraph ">Gestion des commandes</p></li>
+            </ul>
+       
+        <h3 class="text-center fw-semibold  my-4">Fonctionnalités MLP (Minimum Lovable Product)</h3>
+            <ul>
+                <li><p class="dynamic-paragraph ">Génération d’un lien d’accès à la commande de chaque carte de restaurant via un QR code</p></li>
+                <li><p class="dynamic-paragraph ">Responsive</p></li>
+                <li><p class="dynamic-paragraph ">Paramétrage de la charte graphique du restaurant pour sa carte</p></li>
+                <li><p class="dynamic-paragraph ">Paiement en ligne via API Stripe (avantages : package Laravel cashier, voir doc)</p></li>
+            </ul>
+
+
+        <h3 class="text-center fw-semibold  my-4">Contraintes techniques</h3>
+            <ul>
+                <li><p class="dynamic-paragraph ">Laravel</p></li>
+                <li><p class="dynamic-paragraph ">Déploiement sur un nom de domaine</p></li>
+                <li><p class="dynamic-paragraph ">Intégration continue/p></li>
+                <li><p class="dynamic-paragraph ">Gérer l’optimisation des fichiers de production</p></li>
+                <li><p class="dynamic-paragraph ">Template</p></li>
+                <li><p class="dynamic-paragraph ">Certificat SSL (HTTPs)</p></li>
+                <li><p class="dynamic-paragraph ">Tests unitaires</p></li>
+            </ul>
+
+
+        <h3 class="text-center fw-semibold  my-4 pb-2">Moyens utilisés</h3>
+        <div class="projects-icon-list d-flex justify-content-center pb-4">
+            <img src="/public/img/icons/logo-laravel.svg" alt="" />
+            <img src="/public/img/icons/logo-mysql.png" alt="" />
+            <img src="/public/img/icons/logo-tailwind.svg" alt="" />
+        </div>
+   
+        <div class="d-flex justify-content-center  pb-4">
+            <a href="https://clickneat.mattisbabin.fr" target="_blank" class="text-white fw-medium dynamic-paragraph bg-blue p-3 m-1 rounded-3 land-icons border-0">Acceder au site</a>
+        </div>
+    </div>
+</section>
--- view/projets/ovnyl/ovnyl.php
+++ view/projets/ovnyl/ovnyl.php
@@ -2,20 +2,22 @@
     <h1 class="text-center fw-semibold mt-4 mb-4 pt-2 page-title">Ovnyl</h1>
     
     <div class="col-12 d-flex">
-        <img class="col-11 col-xl-6 mt-4 m-auto project-img rounded-4" src="/public/img/projects/ovnyl.png" alt="">
+        <img class="col-11 col-xl-6 mt-4 m-auto project-img rounded-4" src="/public/img/projects/ovnyl.jpg" alt="">
     </div>
   
     <h3 class="text-center fw-semibold pt-4">Tâches réalisées</h3>
         <ul class="dynamic-paragraph d-flex flex-column justify-content-center align-items-center p-0 m-auto text-center">
-            <li class="d-flex"><p>Débug php 5.4 vers php 8.2</p></li>
+            <li class="d-flex"><p>Débug php 5.4 vers php 8.4</p></li>
+            
             <li class="d-flex"><p>Intégration maquette sur toutes les pages</p></li>
             <li class="d-flex"><p>Refonte système de commande / paiement</p></li>
-            <li class="d-flex"><p>Refactorisation partielle panel admin</p></li>
+            <li class="d-flex"><p>Refactorisation globale du projet</p></li>
+            <li class="d-flex"><p></p></li>
         </ul>
 
         <h3 class="text-center fw-semibold pt-2">Contexte</h3>
         <p class="dynamic-paragraph text-center">
-            Projet pour un client à <a href="./entreprise">Akoufen</a>, refonte totale front-end via maquette Figma + mise à jour du code vers PHP 8.2.</p>
+            Projet pour un client à <a href="/entreprise">Akoufen</a>, refonte totale front-end via maquette Figma + mise à jour du code vers PHP 8.4.</p>
     <h3 class="text-center fw-semibold pt-2 pb-2">Moyens utilisés</h3>
         <div class="projects-icon-list d-flex justify-content-center  pb-4">
         <img class="me-2" src="/public/img/icons/logo-php.png" alt="logo-php">
@@ -25,7 +27,7 @@
         </div>
    
     <div class="d-flex justify-content-center  pb-4">
-        <a href="https://www.compagniecotecour.fr/" target="_blank" class="text-white fw-medium dynamic-paragraph bg-blue p-3 m-1 rounded-3 land-icons border-0">Acceder au site</a>
+        <a href="https://ovnyl.com" target="_blank" class="text-white fw-medium dynamic-paragraph bg-blue p-3 m-1 rounded-3 land-icons border-0">Acceder au site</a>
     </div>
         
 </section>
