e42e35bf51ba81ccb86abb19106a76d199c3e817
Mattisbbn <mattisbbn@gmail.com>
2025-04-22 09:11:45 +0200

    upd

--- controller/router.php
+++ controller/router.php
@@ -76,6 +76,18 @@ $router->map('GET', '/realisations', function() {
     require_once 'view/partials/footer.php';
 }, 'realisations');
 
+$router->map('GET', '/realisations/[*:slug]', function($slug) {
+    require_once 'view/partials/header.html';
+    if(file_exists("view/realisations/{$slug}/{$slug}.php")) {
+        require_once("view/realisations/{$slug}/{$slug}.php");
+    } else {
+        require_once("view/404/404.php");
+        header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+        exit;
+    }
+    require_once 'view/partials/footer.php';
+}, 'realisations.project');
+
 $match = $router->match();
 
 if ($match && is_callable($match['target'])) {
